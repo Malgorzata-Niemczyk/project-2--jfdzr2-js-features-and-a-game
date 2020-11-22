8b7f595f856d261a3dce6dab912fc7f883c36746
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2020-11-22 19:03:28 +0100

    add event listeners for the buttons to make it ready for the functions; add nextSlide and prevSlide function to get them ready for the function to write in

--- Project-2/image_slider/app.js
+++ Project-2/image_slider/app.js
@@ -6,3 +6,11 @@ const slideElements = document.querySelectorAll('.slide-item');
 //buttons
 const prevBtnElement = document.querySelector('.previous-bt');
 const nextBtnElement = document.querySelector('.next-btn');
+
+//Functions
+
+
+
+//Buttons events 
+nextBtnElement.addEventListener('click', nextSlide)
+prevBtnElement.addEventListener('click', prevSlide)
