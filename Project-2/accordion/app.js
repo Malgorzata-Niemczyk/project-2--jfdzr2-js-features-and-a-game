b941795fe7ae8c5fa4fe1c827256d95747fe54d0
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2020-11-24 14:03:29 +0100

    within the listener function for the accordionHeaderElement create another variable named accordionBodyElements and add an if statement to check whether the accordionBodyElements variable contains the class of active and if so, add a scrollHeight to it to see the content of the element that is below theaccordionHeaderElement

--- Project-2/accordion/app.js
+++ Project-2/accordion/app.js
@@ -2,6 +2,14 @@ const accordionHeaderElements = document.querySelectorAll('.accordion-item-heade
 
 accordionHeaderElements.forEach(accordionHeaderElement => {
     accordionHeaderElement.addEventListener('click', () => {
-        accordionHeaderElement.classList.toggle('active')
+        accordionHeaderElement.classList.toggle('active');
+
+        const accordionBodyElements = accordionHeaderElement.nextElementSibling;
+
+        if (accordionHeaderElement.classList.contains('active')) {
+            accordionBodyElements.style.maxHeight = accordionBodyElements.scrollHeight + 'px';
+        } else {
+            accordionBodyElements.style.maxHeight = 0;
+        }
     })
 })
