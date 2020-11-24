cfe99290c6a056dc5c17bfab53bb7657d4760d06
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2020-11-24 19:38:14 +0100

    assign zero to the counter variable; update the name of the added and removed classes in the functions

--- Project-2/image_slider/app.js
+++ Project-2/image_slider/app.js
@@ -10,35 +10,35 @@ const slideElementsArr = Array.from(slideElements);
 const prevBtnElement = document.querySelector('.previous-btn');
 const nextBtnElement = document.querySelector('.next-btn');
 
-let counter = 1;
+let counter = 0;
 const slideWidth = 965;
 
 //Functions
 function nextSlide() {
     if (counter >= slideElementsArr.length -1) return;
-    slidesWrapper.classList.add('slides-carousel-transition');
-    textElementsArr.forEach(el => {el.classList.add('animated-content')});
+    slidesWrapper.classList.add('transition-effect');
+    textElementsArr.forEach(el => {el.classList.add('animation-effect')});
     counter++;
     slidesWrapper.style.transform = `translateX(${-slideWidth * counter}px)`;
 };
 
 function prevSlide() {
     if (counter <= 0) return;
-    slidesWrapper.classList.add('slides-carousel-transition');
-    textElementsArr.forEach(el => {el.classList.add('animated-content')});
+    slidesWrapper.classList.add('transition-effect');
+    textElementsArr.forEach(el => {el.classList.add('animation-effect')});
     counter--;
     slidesWrapper.style.transform = `translateX(${-slideWidth * counter}px)`;
 }
 
 function transitionLoop() {
     if (slideElementsArr[counter].id === 'slide-last-copy') {
-        slidesWrapper.classList.remove('slides-carousel-transition');
+        slidesWrapper.classList.remove('transition-effect');
         counter = slideElementsArr.length -2;
         slidesWrapper.style.transform = `translateX(${-slideWidth * counter}px)`;
     }
 
     if (slideElementsArr[counter].id === 'slide-first-copy') {
-        slidesWrapper.classList.remove('slides-carousel-transition');
+        slidesWrapper.classList.remove('transition-effect');
         counter = slideElementsArr.length - counter;
         slidesWrapper.style.transform = `translateX(${-slideWidth * counter})`;
     }
