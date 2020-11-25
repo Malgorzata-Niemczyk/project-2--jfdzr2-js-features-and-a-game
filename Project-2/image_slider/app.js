f3ab82d1bf26668ff51ccf1095f193d373ce44fc
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2020-11-25 19:39:14 +0100

    add animate() method to the slidesWrapper variable within event listener function on buttons so when moving the slide towards left or right the opacity of the images changes

--- Project-2/image_slider/app.js
+++ Project-2/image_slider/app.js
@@ -13,23 +13,44 @@ const nextBtnElement = document.querySelector('.next-btn');
 let counter = 0;
 const slideWidth = 965;
 
-//Functions
+//Functions to change images left and right when clicking on arrow buttons
 function nextSlide() {
     if (counter >= slideElementsArr.length -1) return;
+
     slidesWrapper.classList.add('transition-effect');
-    textElementsArr.forEach(el => {el.classList.add('animation-effect')});
+
+    slidesWrapper.animate([
+        {opacity:'0.1'}, 
+        {opacity:'1.0'}
+    ], {
+        duration:1000, 
+        fill:'forwards'
+    });
+
+    //textElementsArr.forEach(el => {el.classList.add('animation-effect')});
     counter++;
     slidesWrapper.style.transform = `translateX(${-slideWidth * counter}px)`;
 };
 
 function prevSlide() {
     if (counter <= 0) return;
+
     slidesWrapper.classList.add('transition-effect');
-    textElementsArr.forEach(el => {el.classList.add('animation-effect')});
+    
+    slidesWrapper.animate([
+        {opacity:'0.1'}, 
+        {opacity:'1.0'}
+    ], {
+        duration:1000, 
+        fill:'forwards'
+    });
+
+    //textElementsArr.forEach(el => {el.classList.add('animation-effect')});
     counter--;
     slidesWrapper.style.transform = `translateX(${-slideWidth * counter}px)`;
 }
 
+//function to change images in a loop
 function transitionLoop() {
     if (slideElementsArr[counter].id === 'slide-last-copy') {
         slidesWrapper.classList.remove('transition-effect');
