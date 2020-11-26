de948d122c690d12422ec0d8c39737055427b7db
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2020-11-26 16:09:41 +0100

    delete the variables named textElements and textElementsArr; rename the variable named slidesWrapper into slidesCarouselWrapper and update the functions with this variable name accordingly; change the assigned value to the variable named slideWidth

--- Project-2/image_slider/app.js
+++ Project-2/image_slider/app.js
@@ -1,27 +1,24 @@
-const slidesWrapper = document.querySelector('.slides-carousel');
-const textElements = document.querySelectorAll('.content');
-const textElementsArr = Array.from(textElements);
+const slidesCarouselWrapper = document.querySelector('.slides-carousel');
 
 //background images 
 const slideElements = document.querySelectorAll('.slide-item');
-const slideElementsArr = Array.from(slideElements);
 
 //buttons
 const prevBtnElement = document.querySelector('.previous-btn');
 const nextBtnElement = document.querySelector('.next-btn');
 
 let counter = 0;
-const slideWidth = 965;
+const slideWidth = slideElements[0].clientWidth;
 
 //Functions to change images left and right when clicking on arrow buttons
 function nextSlide() {
-    if (counter === slideElementsArr.length) {
+    if (counter === slideElements.length) {
         counter = -1;
     };
 
-    slidesWrapper.classList.add('transition-effect');
+    slidesCarouselWrapper.classList.add('transition-effect');
 
-    slidesWrapper.animate([
+    slidesCarouselWrapper.animate([
         {opacity:'0.1'}, 
         {opacity:'1.0'}
     ], {
@@ -29,9 +26,8 @@ function nextSlide() {
         fill:'forwards'
     });
 
-    //textElementsArr.forEach(el => {el.classList.add('animation-effect')});
     counter++;
-    slidesWrapper.style.transform = `translateX(${-slideWidth * counter}px)`;
+    slidesCarouselWrapper.style.transform = `translateX(${-slideWidth * counter}px)`;
 };
 
 function prevSlide() {
@@ -39,9 +35,9 @@ function prevSlide() {
         counter = 7;
     };
 
-    slidesWrapper.classList.add('transition-effect');
+    slidesCarouselWrapper.classList.add('transition-effect');
     
-    slidesWrapper.animate([
+    slidesCarouselWrapper.animate([
         {opacity:'0.1'}, 
         {opacity:'1.0'}
     ], {
@@ -49,27 +45,26 @@ function prevSlide() {
         fill:'forwards'
     });
 
-    //textElementsArr.forEach(el => {el.classList.add('animation-effect')});
     counter--;
-    slidesWrapper.style.transform = `translateX(${-slideWidth * counter}px)`;
+    slidesCarouselWrapper.style.transform = `translateX(${-slideWidth * counter}px)`;
 }
 
 //function to change images in a loop
 function transitionLoop() {
-    if (slideElementsArr[counter].id === 'slide-last-copy') {
-        slidesWrapper.classList.remove('transition-effect');
-        counter = slideElementsArr.length -2;
-        slidesWrapper.style.transform = `translateX(${-slideWidth * counter}px)`;
+    if (slideElements[counter].id === 'slide-last-copy') {
+        slidesCarouselWrapper.classList.remove('transition-effect');
+        counter = slideElements.length -2;
+        slidesCarouselWrapper.style.transform = `translateX(${-slideWidth * counter}px)`;
     }
 
-    if (slideElementsArr[counter].id === 'slide-first-copy') {
-        slidesWrapper.classList.remove('transition-effect');
-        counter = slideElementsArr.length - counter;
-        slidesWrapper.style.transform = `translateX(${-slideWidth * counter})`;
+    if (slideElements[counter].id === 'slide-first-copy') {
+        slidesCarouselWrapper.classList.remove('transition-effect');
+        counter = slideElements.length - counter;
+        slidesCarouselWrapper.style.transform = `translateX(${-slideWidth * counter})`;
     }
 }
 
 //Buttons events 
 nextBtnElement.addEventListener('click', nextSlide);
 prevBtnElement.addEventListener('click', prevSlide);
-slidesWrapper.addEventListener('transitionend', transitionLoop);
+slidesCarouselWrapper.addEventListener('transitionend', transitionLoop);
