19cbe35d5cc607ccf12aa327f8afa7cef60a7d9e
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2020-12-02 14:07:52 +0100

    create openPanel function that opens the note panel element upon clicking the add button and pin this function to the click listener on the addBtn element to trigger it; create closePanel function that will close the note panel upon clicking the cancel button within this note panel element, to trigger this function pin it to the event listener on the cancelBtn element

--- Project-2/notepad/app.js
+++ Project-2/notepad/app.js
@@ -4,7 +4,7 @@ const deleteNoteBtn = document.getElementsByClassName('delete-note');
 const saveBtn = document.querySelector('.save');
 const cancelBtn = document.querySelector('.cancel');
 
-const noteArea = ocument.querySelector('.note-area');
+const noteArea = document.querySelector('.note-area');
 const notePanel = document.querySelector('.note-panel');
 const categorySelection = document.querySelector('#category');
 const textArea = document.querySelector('#text'); 
@@ -12,3 +12,20 @@ const errorMessage = document.querySelector('.error');
 
 let $selectedValue;
 let $cardID = 0;
+
+//function for opening the note panel element when clicking the add button
+function openPanel() {
+    notePanel.style.display = 'flex';
+};
+
+/*function for closing the note panel element when clicking the cancel button within this element; the function will also clear the inserted content in the text area element and clear the chosen category when we try to open the panel again*/
+function closePanel() {
+    notePanel.style.display = 'none';
+    categorySelection.selectedIndex = 0;
+    textArea.value = '';
+    errorMessage.style.visibility = 'hidden';
+};
+
+//event listeners
+addBtn.addEventListener('click', openPanel);
+cancelBtn.addEventListener('click', closePanel)
