d1b34fa71c9f9a659593b60808c7068153ce76be
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2020-12-02 13:38:16 +0100

    grab an element with a class of note-area that is a parent element where the notes content is added; grab the element with a class of note-panel as it is an element that is going to show in the moment of clicking the add button

--- Project-2/notepad/app.js
+++ Project-2/notepad/app.js
@@ -3,3 +3,6 @@ const deleteAllBtn = document.querySelector('.delete-all');
 const deleteNoteBtn = document.getElementsByClassName('delete-note');
 const saveBtn = document.querySelector('.save');
 const cancelBtn = document.querySelector('.cancel');
+
+const noteArea = ocument.querySelector('.note-area');
+const notePanel = document.querySelector('.note-panel');
