9b6e6767baad6068a7044952688b29a627ac0c45
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2020-12-02 12:43:36 +0100

    create five variables that with querlySelector grab all the different button elements

---
+++ Project-2/notepad/app.js
@@ -0,0 +1,5 @@
+const addBtn = document.querySelector('.add');
+const deleteAllBtn = document.querySelector('.delete-all');
+const deleteNoteBtn = document.querySelector('.delete-note');
+const saveBtn = document.querySelector('.save');
+const cancelBtn = document.querySelector('.cancel');
