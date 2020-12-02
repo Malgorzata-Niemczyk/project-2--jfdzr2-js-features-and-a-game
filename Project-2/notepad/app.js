a7044c2be2b59f5754ef68a56b0c409b782d66e5
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2020-12-02 13:00:54 +0100

    edit the method of grabbing delete button element that is stored in the deleteNoteBtn: instead of querySelector, use getElementsByClassName to be able to grab this element dinamically when a new note is added

--- Project-2/notepad/app.js
+++ Project-2/notepad/app.js
@@ -1,5 +1,5 @@
 const addBtn = document.querySelector('.add');
 const deleteAllBtn = document.querySelector('.delete-all');
-const deleteNoteBtn = document.querySelector('.delete-note');
+const deleteNoteBtn = document.getElementsByClassName('delete-note');
 const saveBtn = document.querySelector('.save');
 const cancelBtn = document.querySelector('.cancel');
