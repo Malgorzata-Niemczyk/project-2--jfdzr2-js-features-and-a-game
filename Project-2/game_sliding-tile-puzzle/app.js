ca37edc0a80669037f2dd75c4edbe00d5c143cdb
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2020-12-29 00:14:27 +0100

    create getPiecePosition function that checks the position of a clicked element and the empty element

--- Project-2/game_sliding-tile-puzzle/app.js
+++ Project-2/game_sliding-tile-puzzle/app.js
@@ -9,5 +9,35 @@ const gameState = [
 
 
 puzzleBoard.addEventListener('click', (event) => {
-    console.log(event.target)
+
+// searching for the clicked element - getting its position, that is, its row and column index
+    function getPiecePosition() {
+        let x;
+        let y; // variables that define the position of a puzzle piece on a vertical and horizontal position; X - is a row number, Y - is a column number
+
+        gameState.forEach((row, rowIndex) => {
+            row.forEach((column, columnIndex) => {
+                if (column === event.target) {
+                    x = rowIndex;
+                    y = columnIndex;
+                }
+            });
+        });
+
+        let emptyX;
+        let emptyY;
+
+        gameState.forEach((row, rowIndex) => {
+            row.forEach((column, columnIndex) => {
+                if (column.innerText === '') {
+                    emptyX = rowIndex;
+                    emptyY = columnIndex;
+                }
+            })
+        });
+        console.log(x, y);
+        console.log(emptyX, emptyY);
+    };
+
+    getPiecePosition()
 })
