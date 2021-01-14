679a569039df15e0756349e44ac75f0a852d0d3c
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-01-14 13:32:55 +0100

    copy the js, html and css files from the accordion folder and paste them to the horizontal accordion folder

---
+++ Project-2/horizontal-accordion/app.js
@@ -0,0 +1,25 @@
+const accordionHeaderElements = document.querySelectorAll('.accordion-item-header');
+
+accordionHeaderElements.forEach(accordionHeaderElement => {
+    accordionHeaderElement.addEventListener('click', () => {
+
+        /*to have one element with a class of accordion-item-body open at a time, and the other element with the same class closing, when clicking an element with a class of accordion-item-header*/
+        const currentlyActiveAccordionHeaderElement = document.querySelector('.accordion-item-header.active');
+        if (currentlyActiveAccordionHeaderElement && currentlyActiveAccordionHeaderElement !== accordionHeaderElement) {
+            currentlyActiveAccordionHeaderElement.classList.remove('active');
+            currentlyActiveAccordionHeaderElement.nextElementSibling.style.maxHeight = 0;
+        }
+
+        //to toggle an active class on an element with a class of accordion-item-header
+        accordionHeaderElement.classList.toggle('active');
+
+        //to hide next slibling, that is an element with a class of accordion-item-body, when the element with a class of acordion-item-header has also an active class
+        const accordionBodyElements = accordionHeaderElement.nextElementSibling;
+
+        if (accordionHeaderElement.classList.contains('active')) {
+            accordionBodyElements.style.maxHeight = accordionBodyElements.scrollHeight + 'px';
+        } else {
+            accordionBodyElements.style.maxHeight = 0;
+        }
+    })
+})
